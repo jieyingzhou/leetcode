17b4fdce2a51a5715d052c938e209dae8b7ef80f
jieyingzhou <36801554+jieyingzhou@users.noreply.github.com>
2021-03-10 16:01:59 -0500

    Time: 0 ms (100.00%), Space: 37.5 MB (89.31%) - LeetHub

---
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] alph = new int[26];
+        for(int i=0; i<order.length(); i++) {
+            alph[order.charAt(i) - 'a'] = i;
+        }
+        for(int i=0; i<words.length; i++) {
+            for(int j=i+1; j<words.length; j++) {
+                int min = Math.min(words[i].length(), words[j].length());
+                for(int k=0; k<min; k++) {
+                    char iChar = words[i].charAt(k);
+                    char jChar = words[j].charAt(k);
+                    if(alph[iChar - 'a'] < alph[jChar - 'a']) {
+                        break;
+                    } else if(alph[iChar - 'a'] > alph[jChar - 'a']) {
+                        return false;
+                    } else if(k == min - 1 && words[i].length() > words[j].length()) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
